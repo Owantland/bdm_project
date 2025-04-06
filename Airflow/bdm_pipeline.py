6aaa2a7104b9ee8c1b6fc0fe6d55fe1ff99a0894
Nicola Mambelli <nicolamambelli@MacBook-Pro-di-Nicola-2.local>
2025-04-06 21:59:47 +0200

    Updated Airflow version with comments

--- Airflow/bdm_pipeline.py
+++ Airflow/bdm_pipeline.py
@@ -5,14 +5,16 @@ import chardet
 import csv
 import json
 import time
+import smtplib
 from datetime import datetime, timedelta
 from tqdm import tqdm
 from azure.storage.blob import BlobServiceClient
 import requests as req
 from airflow.operators.python import PythonOperator
-from airflow.operators.email import EmailOperator
+from airflow.models import XCom
+from airflow.utils.trigger_rule import TriggerRule
 # from airflow.sensors.filesystem import FileSensor
-KEY_PATH = '/Users/path/keys.csv'
+
 folder = 'event_data'
 if not os.path.exists(folder):
     os.makedirs(folder)
@@ -40,6 +42,7 @@ PRS_MON_PATH = os.path.join(folder_place, 'paris_monuments.csv')
 PRS_CUL_PATH = os.path.join(folder_place, 'paris_cultural_places.json')
 
 def azure_connection():
+    KEY_PATH = os.path.join('data', 'keys.csv')
     keys = pd.read_csv(KEY_PATH)
     os.environ["AZURE_CONNECTION_STRING"] = keys["AZURE_CONNECTION_STRING"][0]
     os.environ["RAPID_API_KEY"] = keys["RAPID_API_KEY"][0]
@@ -49,6 +52,88 @@ def azure_connection():
     connection_string = os.getenv('AZURE_CONNECTION_STRING')
     client = BlobServiceClient.from_connection_string(connection_string)
     return client
+
+###################### Make the two diffent function for failer task or complete task##########
+
+def init_progress_task(**context):
+    context['ti'].xcom_push(key='progress_summary', value=[])
+
+def task_complete(context):
+    ti = context['ti']
+    task_id = ti.task_id
+    progress = ti.xcom_pull(key='progress_summary', task_ids='init_progress_task') or []
+    progress.append(f"-Task: {task_id}")
+    XCom.set(
+        key='progress_summary',
+        value=progress,
+        execution_date=ti.execution_date,
+        task_id='init_progress_task',
+        dag_id=ti.dag_id
+    )
+    ti.xcom_push(key='progress_summary', value=progress)
+
+def task_failed(context):
+    ti = context['ti']
+    #is that mail decided to used as Recipient
+    to = 'Recipient@mail.com'
+    task_id = ti.task_id
+    exception = context.get('exception')
+    progress = ti.xcom_pull(key='progress_summary', task_ids='init_progress_task') or []
+    failed = f"-Task: {task_id} FAILED for: {exception}"
+    subject_email_recap = f"Task: {task_id} failed in Airflow"
+
+    if len(progress) >= 2:
+        body_email_recap = (
+            "Airflow recap\n\n"
+            "Tasks complete:\n"
+            + "- " + "\n- ".join(progress)
+            + "\nTask Failed:\n"
+            + failed + "\n"
+        )
+    elif len(progress) == 1:
+        body_email_recap = (
+            "Airflow recap\n\n"
+            "Task complete:\n"
+            + "\n".join(progress)
+            + "\n\nTask Failed:\n"
+            + failed + "\n"
+        )
+    else:
+        body_email_recap = (
+            "Airflow recap\n\n"
+            "Task Failed:\n"
+            + failed + "\n"
+        )
+    
+    send_email(subject_email_recap, body_email_recap, to=[to])
+    progress.append(failed)
+    XCom.set(
+        key='progress_summary',
+        value=progress,
+        execution_date=ti.execution_date,
+        task_id='init_progress_task',
+        dag_id=ti.dag_id
+    )
+    ti.xcom_push(key='progress_summary', value=progress)
+
+def send_email(subject, body, to):
+    #depend about the user used gmail or outlook mail and the port change
+    smtp_port = 25     
+    #depend about the user used gmail or outlook mail ect..            
+    smtp_server = 'smtp.user.com'
+    #is that mail decided to used as Sender
+    smtp_user = 'sender@mail.com'
+    #password of the user (never share)
+    smtp_password = 'password'
+    sender = smtp_user 
+    message = f"Subject: {subject}\n\n{body}"
+
+    with smtplib.SMTP(smtp_server, smtp_port) as server:
+        server.ehlo()            
+        server.starttls()        
+        server.ehlo()            
+        server.login(smtp_user, smtp_password)
+        server.sendmail(sender, to, message)
 
 
 def upload_blob(client, blob_name, local_file_path):
@@ -63,55 +148,79 @@ def upload_blob(client, blob_name, local_file_path):
     raise Exception("Blob was not uploaded")
 
 def import_events():
-  
-    url_event_rome = 'https://preprod.comune.roma.it/catalog9/it/datastore/dump/92699c41-7f59-4064-ba00-a3f2593037d0?bom=True'
-    url_event_paris = 'https://opendata.paris.fr/api/explore/v2.1/catalog/datasets/que-faire-a-paris-/exports/csv?lang=fr&timezone=Europe%2FBerlin&use_labels=true&delimiter=%3B'
-    url_event_barca = 'https://opendata-ajuntament.barcelona.cat/data/dataset/2767159c-1c98-46b8-a686-2b25b40cb053/resource/3abb2414-1ee0-446e-9c25-380e938adb73/download'
-    url_event_madrid = 'https://datos.madrid.es/egob/catalogo/300107-0-agenda-actividades-eventos.csv'
-    # Connection to the azure server
-    client = azure_connection()
-    # event rome
-    res_rome = req.get(url_event_rome)
-    os.makedirs(os.path.dirname(filepath_rome), exist_ok=True)
-    with open(filepath_rome, 'w') as file:
-        file.write(res_rome.text)
-    upload_blob(client, "events/rome_events.csv", filepath_rome)
-    os.remove(filepath_rome)
-
-    # event paris
-    res_paris = req.get(url_event_paris)
-    os.makedirs(os.path.dirname(filepath_paris), exist_ok=True)
-    with open(filepath_paris, 'w') as file:
-        file.write(res_paris.text)
-    upload_blob(client, "events/paris_events.csv", filepath_paris)
-    os.remove(filepath_paris)
-
-    # event barca
-    res_barca = req.get(url_event_barca)
-    os.makedirs(os.path.dirname(filepath_barca), exist_ok=True)
-    with open(filepath_barca, 'wb') as f:
-        f.write(res_barca.content)
-
-    with open(filepath_barca, 'rb') as f:
-        rawdata = f.read()
-        guess = chardet.detect(rawdata)
-
-    encoding_to_use = guess['encoding'] if guess['encoding'] else 'utf-8-sig'
-    content_text = rawdata.decode(encoding_to_use)
-
-    with open(filepath_barca, 'w', encoding='utf-8') as f:
-        f.write(content_text)
-
-    upload_blob(client, "events/barcelona_events.csv", filepath_barca)
-    os.remove(filepath_barca)
-
-    # event madrid
-    res_madrid = req.get(url_event_madrid)
-    os.makedirs(os.path.dirname(filepath_madrid), exist_ok=True)
-    with open(filepath_madrid, 'w') as file:
-        file.write(res_madrid.text)
-    upload_blob(client, "events/madrid_events.csv", filepath_madrid)
-    os.remove(filepath_madrid)
+    # Connection to the Azure server
+    try:
+        client = azure_connection()
+    except Exception as e:
+        print("Error of connection to Azure:", e)
+        raise
+    # Event Rome
+    try:
+        url_event_rome = 'https://preprod.comune.roma.it/catalog9/it/datastore/dump/92699c41-7f59-4064-ba00-a3f2593037d0?bom=True'
+        res_rome = req.get(url_event_rome)
+        os.makedirs(os.path.dirname(filepath_rome), exist_ok=True)
+        with open(filepath_rome, 'w') as file:
+            file.write(res_rome.text)
+        upload_blob(client, "events/rome_events.csv", filepath_rome)
+        os.remove(filepath_rome)
+        print("Rome event processed successfully.")
+    except Exception as e:
+        print("Error processing Rome event:", e)
+        raise
+    
+    # Event Paris
+    try:
+        url_event_paris = 'https://opendata.paris.fr/api/explore/v2.1/catalog/datasets/que-faire-a-paris-/exports/csv?lang=fr&timezone=Europe%2FBerlin&use_labels=true&delimiter=%3B'
+        res_paris = req.get(url_event_paris)
+        os.makedirs(os.path.dirname(filepath_paris), exist_ok=True)
+        with open(filepath_paris, 'w') as file:
+            file.write(res_paris.text)
+        upload_blob(client, "events/paris_events.csv", filepath_paris)
+        os.remove(filepath_paris)
+        print("Paris event processed successfully.")
+    except Exception as e:
+        print("Error processing Paris event:", e)
+        raise
+    
+    # Event Barcelona
+    try:
+        url_event_barca = 'https://opendata-ajuntament.barcelona.cat/data/dataset/2767159c-1c98-46b8-a686-2b25b40cb053/resource/3abb2414-1ee0-446e-9c25-380e938adb73/download'
+        res_barca = req.get(url_event_barca)
+        os.makedirs(os.path.dirname(filepath_barca), exist_ok=True)
+        with open(filepath_barca, 'wb') as f:
+            f.write(res_barca.content)
+    
+        # Rileva la codifica e decodifica il file
+        with open(filepath_barca, 'rb') as f:
+            rawdata = f.read()
+            guess = chardet.detect(rawdata)
+    
+        encoding_to_use = guess['encoding'] if guess['encoding'] else 'utf-8-sig'
+        content_text = rawdata.decode(encoding_to_use)
+    
+        with open(filepath_barca, 'w', encoding='utf-8') as f:
+            f.write(content_text)
+    
+        upload_blob(client, "events/barcelona_events.csv", filepath_barca)
+        os.remove(filepath_barca)
+        print("Barcelona event processed successfully.")
+    except Exception as e:
+        print("Error processing Barcelona event:", e)
+        raise
+    
+    # Event Madrid
+    try:
+        url_event_madrid = 'https://datos.madrid.es/egob/catalogo/300107-0-agenda-actividades-eventos.csv'
+        res_madrid = req.get(url_event_madrid)
+        os.makedirs(os.path.dirname(filepath_madrid), exist_ok=True)
+        with open(filepath_madrid, 'w') as file:
+            file.write(res_madrid.text)
+        upload_blob(client, "events/madrid_events.csv", filepath_madrid)
+        os.remove(filepath_madrid)
+        print("Madrid event processed successfully.")
+    except Exception as e:
+        print("Error processing Madrid event:", e)
+        raise
 
 # Downloads and processes data from the Barcelona Opendata portal through the use of their API
 def dwnld_brcn(client):
@@ -131,7 +240,6 @@ def dwnld_brcn(client):
 
     upload_blob(client, f"cultural_places/brcn_cultural_places.csv", BRCN_PATH)
     os.remove(BRCN_PATH)
-
 
 # Initial download and parsing of Barcelona data
 def lec_ini(results):
@@ -185,8 +293,6 @@ def lec_ini(results):
                 'geo_epgs_4326_lon': record["geo_epgs_4326_lon"]
             })
     csvfile.close()
-
-
 # Parsing of the Barcelona data after the first
 def lec_post(results):
     # Write into CSV file
@@ -238,7 +344,6 @@ def lec_post(results):
                 'geo_epgs_4326_lon': record["geo_epgs_4326_lon"]
             })
     csvfile.close()
-
 
 # Downloads and processes data from the Madrid Opendata portal through the use of their API
 def dwnld_madrid(client):
@@ -298,7 +403,6 @@ def dwnld_madrid(client):
         f.close()
     upload_blob(client, f"cultural_places/madrid_concert_halls.csv", MDR_CON_PATH)
     os.remove(MDR_CON_PATH)
-
 # Downloads and processes data from the Paris Opendata portal through the use of their API
 def dwnld_paris(client):
     url_museums = 'https://data.culture.gouv.fr/api/explore/v2.1/catalog/datasets/musees-de-france-base-museofile/exports/csv?delimiter=%3B&list_separator=%2C&quote_all=false&with_bom=true'
@@ -341,14 +445,18 @@ def dwnld_paris(client):
 
 
 def import_main_point():
+    # Connection to the Azure server
     try:
         client = azure_connection()
+    except Exception as e:
+        print("Error of connection to Azure:", e)
+    try:
         dwnld_brcn(client)
         dwnld_madrid(client)
         dwnld_paris(client)
         print("import_main_point completed successfully")
     except Exception as e:
-        print(f"ERROR in import_main_point: {e}")
+        print(f"ERROR in import main point: {e}")
         raise
 
 def save_images(res, client, querystring, city):
@@ -372,7 +480,7 @@ def save_json(res, filename):
         json.dump(res, file)
 
 
-def import_accomodation_extra_info():
+def import_accomodation_and_weather_info():
     client = azure_connection()
     accomodation_endpoint = "https://booking-com15.p.rapidapi.com/api/v1/hotels/searchHotels"
     weather_endpoint = f'https://api.open-meteo.com/v1/forecast'
@@ -423,10 +531,11 @@ def import_accomodation_extra_info():
                     }
 
     start = datetime.strptime('2025-06-05', "%Y-%m-%d")
-    end = datetime.strptime('2025-06-10', "%Y-%m-%d")
+    end = datetime.strptime('2025-06-06', "%Y-%m-%d")
     last_city = 'Rome'
     try:
         for single_date in tqdm([start + i * delta for i in range((end - start).days + 1)]):
+            
             arrival_date = single_date.strftime("%Y-%m-%d")
             departure_date = (single_date + timedelta(days=1)).strftime("%Y-%m-%d")
             start_date = single_date.replace(year=single_date.year - 1).strftime("%Y-%m-%d")
@@ -476,49 +585,77 @@ def import_accomodation_extra_info():
                 if last_city:
                     last_city = None
 
+            if single_date >= end:
+                break
+                
+
     except Exception as e:
         print(e)
         print(last_city, arrival_date, departure_date)
 
+def recap_task_complete(**context):
+    ti = context['ti']
+    #is that mail decided to used as Recipient
+    to = 'Recipient@mail.com'
+    dag_run = context.get('dag_run')
+    progress = ti.xcom_pull(key='progress_summary', task_ids='init_progress_task') or []
+
+    subject_email_recap = f"DAG: {dag_run.dag_id} Complete in Airflow!"
+    body_email_recap = (
+        f"DAG complete: {dag_run.dag_id}\n\n"
+        f"List of completed tasks:\n"
+        + "\n".join(progress)
+        + "\n\n"
+        "All task is complete!"
+    )
+    send_email(subject_email_recap, body_email_recap, to=[to])
+
 
 default_args = {
     'owner': 'airflow',
-    'start_date': datetime(2024, 1, 1),
+    'start_date': datetime(2025, 1, 1),
     'retries': 1,
+    'depends_on_past': False,
 }
 
 with DAG(
     dag_id='bdm_data_pipeline',
     default_args=default_args,
-    schedule=None,
+    schedule_interval=None,
     catchup=False,
     tags=['bdm', 'events', 'data'],
 ) as dag:
 
-    accomodation_extra = PythonOperator(
-        task_id='import_accomodation_and_extra_info',
-        python_callable=import_accomodation_extra_info
+    init_progress = PythonOperator(
+        task_id='init_progress_task',
+        python_callable=init_progress_task
+    )
+
+    accomodation_and_weather = PythonOperator(
+        task_id='import_accomodation_and_weather_info',
+        python_callable=import_accomodation_and_weather_info,
+        on_success_callback=task_complete,
+        on_failure_callback=task_failed
     )
 
     main_point = PythonOperator(
         task_id='import_main_point',
-        python_callable=import_main_point
+        python_callable=import_main_point,
+        on_success_callback=task_complete,
+        on_failure_callback=task_failed
     )
 
     events = PythonOperator(
         task_id='import_events',
-        python_callable=import_events
+        python_callable=import_events,
+        on_success_callback=task_complete,
+        on_failure_callback=task_failed
     )
 
-    # notify_task = EmailOperator(
-    #     task_id='send_email_notification',
-    #     to= 'your@gmail.com',
-    #     subject='Airflow Notification: DAG complete!',
-    #     html_content="""
-    #             <h3>DAG: {{ dag.dag_id }} completed successfully!</h3>
-    #             <p>Execution finished at: {{ ts }}</p>
-    #             """,
-    #     dag=dag
-    #     )
+    recap = PythonOperator(
+        task_id='recap_task_complete',
+        python_callable=recap_task_complete,
+        trigger_rule=TriggerRule.ALL_DONE
+    )
 
-    main_point >>  events >> accomodation_extra
+    init_progress >> events>> main_point >> accomodation_and_weather >> recap
